7271ecfee0370831e58172162d3402835a44cb94
Kanny <kanny.ghafour87@gmail.com>
2023-10-23 22:07:43 -0500

    lab completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 
+import math
+
+
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -=1
+    print("Happy New Year!")
+
+def fizzbuzz_printer():
+    for num in range(100):
+        print(fizzbuzz(num))
+
+
+def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+
+def reverse_string(str):
+    reversed_str = ""
+    for char in str:
+        reversed_str = char + reversed_str
+    return reversed_str
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num**2 for num in int_list]
 
-def fizzbuzz():
-    # code goes here!
-    pass
